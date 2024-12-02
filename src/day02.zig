847dc0533995529177b27b392387c59ca38c77fc
victor-scarano <157558458+victor-scarano@users.noreply.github.com>
2024-12-02 14:18:22 -0600

    Day 2 (Unfinished)

--- src/day02.zig
+++ src/day02.zig
@@ -3,27 +3,45 @@ const data = @embedFile("data/day02.txt");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
+
     var valid_reports: u16 = 0;
+
     var reports = std.mem.splitScalar(u8, data, '\n');
     while (reports.next()) |report| {
         var levels = std.mem.splitScalar(u8, report, ' ');
-        var last = std.fmt.parseInt(i16, levels.first(), 10) catch break;
-        const increasing = last < try std.fmt.parseInt(i16, levels.peek().?, 10);
-        const is_valid = while (levels.next()) |slice| {
-            const curr = std.fmt.parseInt(i16, slice, 10) catch break false;
-            if ((last == curr) or (increasing != (last < curr)) or (@abs(last - curr) > 3)) {
-                break false;
-            }
-            last = curr;
-        } else true;
 
-        if (is_valid) {
+        var inc: u8 = 0;
+        var dec: u8 = 0;
+        var eq: u8 = 0;
+        var gap: u8 = 0;
+
+        var prev = std.fmt.parseInt(i16, levels.first(), 10) catch break;
+
+        while (levels.next()) |level| {
+            const curr = std.fmt.parseInt(i16, level, 10) catch break;
+
+            if (prev == curr) {
+                eq += 1;
+            } else if (prev < curr) {
+                inc += 1;
+            } else {
+                dec += 1;
+            }
+
+            if (@abs(prev - curr) > 3) {
+                gap += 1;
+            }
+
+            prev = curr;
+        }
+
+        if (@min(inc, dec) + eq + gap == 0) {
             valid_reports += 1;
-            try stdout.print("VALID REPORT: {s}\n", .{report});
         } else {
-            try stdout.print("INVALID REPORT: {s}\n", .{report});
+            // check whats wrong with these
+            try stdout.print("{} | {} | {} | {s}\n", .{ @min(inc, dec), eq, gap, report });
         }
     }
+
     try stdout.print("Part one: {}.\n", .{valid_reports});
 }
-
