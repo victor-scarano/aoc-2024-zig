958e0f2edf2292f2279ff281b7f0deea94d0b01d
victor-scarano <157558458+victor-scarano@users.noreply.github.com>
2024-12-03 14:30:19 -0600

    Day Three Complete

--- src/day03.zig
+++ src/day03.zig
@@ -7,26 +7,6 @@ pub fn main() !void {
     try stdout.print("Part two answer: {d}\n", .{part2()});
 }
 
-fn part2() u32 {
-    var sum: u32 = 0;
-    var dos = std.mem.splitSequence(u8, data, "do()");
-    while (dos.next()) |until_next_do| {
-        var donts = std.mem.splitSequence(u8, until_next_do, "don't()");
-        while (donts.next()) |until_next_dont| {
-            var muls = std.mem.splitSequence(u8, until_next_dont, "mul(");
-            while (muls.next()) |mul| {
-                var end = std.mem.splitScalar(u8, mul, ')');
-                var inside = std.mem.splitScalar(u8, end.first(), ',');
-                const first = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
-                const last = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
-                if (inside.next()) |_| continue;
-                sum += first * last;
-            }
-        }
-    }
-    return sum;
-}
-
 fn part1() u32 {
     var sum: u32 = 0;
     var muls = std.mem.splitSequence(u8, data, "mul(");
@@ -41,3 +21,21 @@ fn part1() u32 {
     return sum;
 }
 
+fn part2() u32 {
+    var sum: u32 = 0;
+    var dos = std.mem.splitSequence(u8, data, "do()");
+    while (dos.next()) |until_next_do| {
+        var donts = std.mem.splitSequence(u8, until_next_do, "don't()");
+        const until_next_dont = donts.first();
+        var muls = std.mem.splitSequence(u8, until_next_dont, "mul(");
+        while (muls.next()) |mul| {
+            var end = std.mem.splitScalar(u8, mul, ')');
+            var inside = std.mem.splitScalar(u8, end.first(), ',');
+            const first = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
+            const last = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
+            if (inside.next()) |_| continue;
+            sum += first * last;
+        }
+    }
+    return sum;
+}
