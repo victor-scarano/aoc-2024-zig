a941a2f5b42c9c66450adfbee59c2e8f51b7233e
victor-scarano <157558458+victor-scarano@users.noreply.github.com>
2024-12-04 11:42:47 -0600

    Day four done!

--- src/day04.zig
+++ src/day04.zig
@@ -1,46 +1,51 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
-
-const data = @embedFile("data/day04.txt");
+const input: []const u8 = @embedFile("data/day04.txt");
 
 pub fn main() !void {
-    
+    const stdout = std.io.getStdOut().writer();
+
+    var part1: usize = 0;
+    var part2: usize = 0;
+    for (0.., input) |idx, char|  {
+        if (char == 'X') part1 += xmas(idx);
+        if (char == 'A' and x_mas(idx)) part2 += 1;
+    }
+
+    try stdout.print("Part one answer: {d}\n", .{part1});
+    try stdout.print("Part two answer: {d}\n", .{part2});
 }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+fn xmas(idx: usize) u8 {
+    var sum: u8 = 0;
+    if (rel(idx, 0, 1) == 'M' and rel(idx, 0, 2) == 'A' and rel(idx, 0, 3) == 'S') sum += 1;
+    if (rel(idx, 1, 1) == 'M' and rel(idx, 2, 2) == 'A' and rel(idx, 3, 3) == 'S') sum += 1;
+    if (rel(idx, 1, 0) == 'M' and rel(idx, 2, 0) == 'A' and rel(idx, 3, 0) == 'S') sum += 1;
+    if (rel(idx, 1, -1) == 'M' and rel(idx, 2, -2) == 'A' and rel(idx, 3, -3) == 'S') sum += 1;
+    if (rel(idx, 0, -1) == 'M' and rel(idx, 0, -2) == 'A' and rel(idx, 0, -3) == 'S') sum += 1;
+    if (rel(idx, -1, -1) == 'M' and rel(idx, -2, -2) == 'A' and rel(idx, -3, -3) == 'S') sum += 1;
+    if (rel(idx, -1, 0) == 'M' and rel(idx, -2, 0) == 'A' and rel(idx, -3, 0) == 'S') sum += 1;
+    if (rel(idx, -1, 1) == 'M' and rel(idx, -2, 2) == 'A' and rel(idx, -3, 3) == 'S') sum += 1;
+    return sum;
+}
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+fn x_mas(idx: usize) bool {
+    var valid = true;
+    const tr = rel(idx, 1, 1);
+    const tl = rel(idx, -1, 1);
+    const br = rel(idx, 1, -1);
+    const bl = rel(idx, -1, -1);
+    if (tr != 'M' and tr != 'S') valid = false;
+    if (tl != 'M' and tl != 'S') valid = false;
+    if (br != 'M' and br != 'S') valid = false;
+    if (bl != 'M' and bl != 'S') valid = false;
+    if (tr == bl) valid = false;
+    if (tl == br) valid = false;
+    return valid;
+}
 
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+fn rel(idx: usize, x: isize, y: isize) ?u8 {
+    const i: isize = @intCast(idx);
+    const absolute: isize = i + (y * 141) + x;
+    if (absolute < 0 or absolute >= input.len) return null;
+    return input[@intCast(absolute)];
+}
