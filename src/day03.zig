576956d0d8faae35845c4e4919645f5a8ab713d2
victor-scarano <157558458+victor-scarano@users.noreply.github.com>
2024-12-03 20:11:12 -0600

    Update

--- src/day03.zig
+++ src/day03.zig
@@ -1,46 +1,43 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
-
 const data = @embedFile("data/day03.txt");
 
 pub fn main() !void {
-    
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Part one answer: {d}\n", .{part1()});
+    try stdout.print("Part two answer: {d}\n", .{part2()});
 }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+fn part2() u32 {
+    var sum: u32 = 0;
+    var dos = std.mem.splitSequence(u8, data, "do()");
+    while (dos.next()) |until_next_do| {
+        var donts = std.mem.splitSequence(u8, until_next_do, "don't()");
+        while (donts.next()) |until_next_dont| {
+            var muls = std.mem.splitSequence(u8, until_next_dont, "mul(");
+            while (muls.next()) |mul| {
+                var end = std.mem.splitScalar(u8, mul, ')');
+                var inside = std.mem.splitScalar(u8, end.first(), ',');
+                const first = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
+                const last = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
+                if (inside.next()) |_| continue;
+                sum += first * last;
+            }
+        }
+    }
+    return sum;
+}
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+fn part1() u32 {
+    var sum: u32 = 0;
+    var muls = std.mem.splitSequence(u8, data, "mul(");
+    while (muls.next()) |mul| {
+        var end = std.mem.splitScalar(u8, mul, ')');
+        var inside = std.mem.splitScalar(u8, end.first(), ',');
+        const first = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
+        const last = std.fmt.parseInt(u32, inside.next().?, 10) catch continue;
+        if (inside.next()) |_| continue;
+        sum += first * last;
+    }
+    return sum;
+}
 
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
